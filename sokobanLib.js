0502a9e51af055ba9b0493764b6d287c998daa33
Rohan11446 <rharlal@qualogy.com>
2025-09-25 14:13:19 -0300

    Delete sokobanLib.js

--- sokobanLib.js
+++
@@ -1,7 +0,0 @@
-import generate from "sokoban-generator";
-
-// Optional warmup to speed up first request
-generate({ width: 5, height: 5, boxes: 1, attempts: 10, type: "string" });
-console.log("Sokoban generator warmed up");
-
-export default generate;
